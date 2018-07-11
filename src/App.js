30b7597f7af1c672118355f5a62fe33095e4f3d6
Kyle Bradford <kfoxb@kfoxb.com>
2018-07-11 13:04:39 -0600

    fix linting errors in App

--- src/App.js
+++ src/App.js
@@ -3,40 +3,48 @@ import DancingGuava from './DancingGuava';
 import VideoPlayer from './VideoPlayer';
 
 class App extends Component {
-  static getRandomCoord = () => Math.random() * 100;
-
   state = {
     bassDropped: false,
   }
 
+  static getRandomCoord = () => Math.random() * 100;
+
   getDancers = () => {
-    if (this.state.bassDropped) {
+    const { bassDropped } = this.state;
+    if (bassDropped) {
       setTimeout(() => this.forceUpdate(), 1000);
-      let res = [];
+      const res = [];
       const e = document.documentElement;
       const g = document.body;
       const x = window.innerWidth || e.clientWidth || g.clientWidth;
-      const y = window.innerHeight|| e.clientHeight|| g.clientHeight;
+      const y = window.innerHeight || e.clientHeight || g.clientHeight;
       // firefox seems to be a bit slower, this lowers the total dancers
-      const dancerArea = navigator.userAgent.includes('Firefox') ? 240000 : 120000 
+      const dancerArea = navigator.userAgent.includes('Firefox') ? 240000 : 120000;
       const dancers = x * y / dancerArea;
-      for (let i = 0; i < dancers; i++) {
-        const x = App.getRandomCoord();
-        res.push((<DancingGuava delay={x * -1}key={x} x={x} y={App.getRandomCoord()}/>))
+      for (let i = 0; i < dancers; i += 1) {
+        const xCoords = App.getRandomCoord();
+        res.push((
+          <DancingGuava
+            delay={xCoords * -1}
+            key={xCoords}
+            x={xCoords}
+            y={App.getRandomCoord()}
+          />));
       }
       return res;
     }
     return (
-      <DancingGuava x={50} y={50}/>
+      <DancingGuava x={50} y={50} />
     );
   }
 
   render() {
+    const { bassDropped } = this.state;
     return (
       <Fragment>
         <VideoPlayer
-          bassDropped={this.state.bassDropped}
-          setBassDropped={(bassDropped) => this.setState({ bassDropped })}
+          bassDropped={bassDropped}
+          setBassDropped={isDropped => this.setState({ bassDropped: isDropped })}
         />
         { this.getDancers() }
       </Fragment>

664a35c65b919a16b0fe7444f721f087aa8d3a01
Kyle Bradford <kfoxb@kfoxb.com>
2018-07-11 11:32:56 -0600

    dynamically add youtube player_api script

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -16,6 +16,14 @@ export default class VideoPlayer extends Component {
     };
   }
 
+  componentDidMount() {
+    const tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  }
+
   componentDidUpdate(prevProps, prevState) {
     if (!prevState.ready && this.state.ready) {
       this.initPlayer();
